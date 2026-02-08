6da8866d0d20d4ed2c526cd1cbd76b03c115f34d
Koba-Tetsu <Koba-Tetsu@users.noreply.github.com>
2026-02-08 16:59:43 +0000

    deploy: slidev build (a186cfcc6bd1ed3d8adc9102cf06599ddc1adf17) Koba-Tetsu/slidev-study@a186cfcc6bd1ed3d8adc9102cf06599ddc1adf17

--- assets/md-Bz2MaV61.js
+++
@@ -1,8 +0,0 @@
-import{o as a,b as l,w as r,g as c,v as o,x as d,T as e}from"./modules/vue-C_Vekkm4.js";import{I as n}from"./slidev/default-BH9Xde5y.js";import{u as v,f as m}from"./slidev/context-DyRFOmNa.js";import"./index-CeqVy7zu.js";import"./modules/shiki-HS4eTO4c.js";const B={__name:"slides.md__slidev_10",setup(p){const{$clicksContext:t,$frontmatter:i}=v();return t.setup(),(k,s)=>(a(),l(n,o(d(e(m)(e(i),9))),{default:r(()=>[...s[0]||(s[0]=[c("h2",null,"「このスライドに1行足す」とこう変わる",-1),c("div",{class:"grid-2 big"},[c("div",{class:"card"},[c("div",{class:"muted"},"（元のコード）"),c("pre",{class:"code"},`- <v-click>前提</v-click>
-- <v-click>理由</v-click>
-- <v-click>結論</v-click>
-    `)]),c("div",{class:"card"},[c("div",{class:"muted"},"（1行足したコード）"),c("pre",{class:"code"},`- <v-click>前提</v-click>
-- <v-click>補足</v-click>
-- <v-click>理由</v-click>
-- <v-click>結論</v-click>
-    `)])],-1),c("div",{class:"card muted center",style:{"margin-top":"12px"}}," アニメ順の調整を「テキスト編集」で済ませられる ",-1)])]),_:1},16))}};export{B as default};
