f859db71993a8fe5c7c689e4ffc8831f32072f18
Koba-Tetsu <Koba-Tetsu@users.noreply.github.com>
2026-02-08 14:40:51 +0000

    deploy: slidev build (a57fb6bec40ef6eb690501ae17cdfd5d2d3cd0e1) Koba-Tetsu/slidev-study@a57fb6bec40ef6eb690501ae17cdfd5d2d3cd0e1

---
+++ assets/md-OARrmn4J.js
@@ -0,0 +1,6 @@
+import{o as l,b as o,w as n,g as e,v as i,x as d,T as t}from"./modules/vue-C_Vekkm4.js";import{I as u}from"./slidev/default-BH9Xde5y.js";import{u as c,f as p}from"./slidev/context-DyRFOmNa.js";import"./index-CeqVy7zu.js";import"./modules/shiki-HS4eTO4c.js";const P={__name:"slides.md__slidev_20",setup(m){const{$clicksContext:r,$frontmatter:a}=c();return r.setup(),(f,s)=>(l(),o(u,i(d(t(p)(t(a),19))),{default:n(()=>[...s[0]||(s[0]=[e("h2",null,"発表者しか見えない原稿（Speaker Notes）",-1),e("div",{class:"grid-2 big"},[e("div",{class:"card"},[e("div",{class:"muted"},"（スライドにこう書く）"),e("pre",{class:"code"},`<!--
+ここに発表原稿を書く
+聴衆には表示されない
+Presenter Modeでだけ見える
+-->
+    `)]),e("div",{class:"card"},[e("strong",null,"発表者は"),e("ul",null,[e("li",null,"次のスライド"),e("li",null,"タイマー"),e("li",null,"ノート（原稿）")]),e("div",{class:"muted",style:{"margin-top":"12px"}}," /presenter/ を開くと見える ")])],-1)])]),_:1},16))}};export{P as default};
