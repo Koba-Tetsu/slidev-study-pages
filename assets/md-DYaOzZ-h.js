bf9460f316331624391fceb09feeb2cf3af3e15d
Koba-Tetsu <Koba-Tetsu@users.noreply.github.com>
2026-02-09 00:51:01 +0000

    deploy: slidev build (1b5069c4096404808aec711d84f837e4dc1294d1) Koba-Tetsu/slidev-study@1b5069c4096404808aec711d84f837e4dc1294d1

--- assets/md-DYaOzZ-h.js
+++
@@ -1,7 +0,0 @@
-import{o as r,b as o,w as i,g as s,v as l,x as d,T as a}from"./modules/vue-B43nByi5.js";import{I as n}from"./slidev/default-CI4tFqx4.js";import{u as p,f as m}from"./slidev/context-2fObImVu.js";import"./index-Ci7CRIy1.js";import"./modules/shiki-CnOc2oPr.js";const B={__name:"slides.md__slidev_12",setup(u){const{$clicksContext:c,$frontmatter:t}=p();return c.setup(),(v,e)=>(r(),o(n,l(d(a(m)(a(t),11))),{default:i(()=>[...e[0]||(e[0]=[s("h2",null,"MarpとSlidevの違い（書き方）",-1),s("div",{class:"grid-2 code-demo big"},[s("div",{class:"card"},[s("div",{class:"muted"},"Marp"),s("pre",{class:"code"},`- 前提
-- 理由
-- 結論
-    `),s("p",{class:"muted"}," すべて最初から見える ")]),s("div",{class:"card"},[s("div",{class:"muted"},"Slidev"),s("pre",{class:"code"},`- <v-click>前提</v-click>
-- <v-click>理由</v-click>
-- <v-click>結論</v-click>
-    `),s("p",{class:"muted"}," 出す順番を設計できる ")])],-1)])]),_:1},16))}};export{B as default};
